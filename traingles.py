03952fe0ede0bc3437e4ec062011c81bafab36f0
harish4459 <harish.sunny9@gmail.com>
2022-07-12 10:51:45 +0530

    triangles.py

---
+++ traingles.py
@@ -0,0 +1,99 @@
+print star patterns
+
+num = 5
+
+for i in range(1,num+1):
+    print ("* "*i)
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+    
+    
+for i in range(1,num+1):
+    print (" "*(num -i)+"* "*i)
+    
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+  
+
+for i in range(1,num+1):
+    print ("  "*(num -i)+"* "*i)
+    
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+
+for i in range(1,num+1):
+    print (" "*(num -i)+"* "*i)
+for i in range(num,0,-1):
+    print (" "*(num -i)+"* "*i)
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+
+
+for i in range(1,num+1):
+    print (" "*(num -i)+"* "*i)
+for i in range(num-1,0,-1):
+    print (" "*(num -i)+"* "*i)
+    
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+
+
+for i in range(num,0,-1):
+    print ("  "*(num -i)+"* "*i)
+    
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+  
+ 
+
+for i in range(num,0,-1):
+    print ("* "*i)
+    
+* * * * *
+* * * *
+* * *
+* *
+*
+ 
+
+
+
+
+
+
+
+
+
